11651778215267bfb43a26eeefd8ccb1d500ea65
PedroEgg <pedro.eggg@gmail.com>
2025-07-19 18:18:56 -0300

    idea

--- app/main.py
+++ app/main.py
@@ -28,10 +28,19 @@ app.templates_auto_reload = True
 logger.info('init completed!')
 # ------------------------------
 
-# add redis to docker-compose
-# configure redis
-# implement redis library
+# add and configure redis on the environment
+# implement redis library for queue (LIST, LPUSH, BRPOP) and for caching (GET, SET, INCR, EXPIRE)
 # implement flask-smorest + marshmallow
-# implement service
-# implement repository
-# implement model
+# implement service to use repository inserting (queue) and get data into/from redis
+# implement repository to use redis
+# implement model for responses and payloads
+
+# adjust/configure uWSGI
+# use redis as a queue integrated with the both app instances (read-only) to handle instabilities and avoiding inconsistency
+# insert into redis the payments to be processed and let one or more consumers handle them (with tenacity retrying + using the fallback payment processor)
+# save/update on redis after each payment processing, then the summary will always be correct for both app1 and app2 APIs
+
+
+# another option would be to use the app1 and app2 to handle the payment processing with yours workers
+# since we can configure uWSGI to have a lot of workers, maybe we can process a lot of requests simultaneously
+# but then I will need to worry about bottlenecks, because if the payments processors become unavailable for so long I'm going to have problems
